52927cc07579a9806c1f29313378383dddb3477c
0J3lack0 <46741270+0J3lack0@users.noreply.github.com>
2019-01-16 16:05:06 +0300

    Update script.py

--- script.py
+++ script.py
@@ -2,10 +2,20 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 url = 'https://www.cfa.harvard.edu/shocks/ac_master_data/ac_master_1998.html'
-
+url2 ='https://www.cfa.harvard.edu/shocks/ac_master_data/'
 r = requests.get(url)
 
 soup = bs(r.content, 'html.parser')
-soup.find('table', attrs={'class': 'content_table'}).find_all('a')
+v = soup.find('table', attrs={'class': 'content_table'}).find_all('a')
+v.pop()   #array'deki son link çıkarıtılır.
 
 
+#örneğin   v[1] = <a href="./00000/ac_master_00000.html">link</a> bize ilk elemanda ki  ./00000/ac_master_00000.html  kısım lazım ve bunu tüm array elemanları için uygulamalıyız
+#v.attrs['href'] istediğimiz işlemi yapacaktır.
+
+for i in range(0,len(v)):
+	v[i] = v[i].attrs['href']
+  
+  #url lerimizi oluşturalım
+for j in range(0,len(v))
+     u[j] = url2+v[j]
