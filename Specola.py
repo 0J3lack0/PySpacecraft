1d15c76e737375564d55d1d36be3ddb8a9796d2d
ÇGani Çağlar Çoban <g.caglar.c@gmail.com>
2019-01-23 23:44:30 +0300

    Gereksiz modüller kaldırılarak satırb boşluları düzenlendi.

--- Specola.py
+++ Specola.py
@@ -1,13 +1,10 @@
 import requests #project:Specola ; Specola gözlem evine ait güneş leke çizimlerinin  indirilmesi.
-import xlsxwriter
-import os
-import urllib
 from bs4 import BeautifulSoup as bs
 
 
 baseURL = 'http://www.specola.ch/drawings/e-cal'
-years   = ['1995', '1996', '1997','1998','1999','2000','2001','2002', '2003','2004','2005','2006',
-              '2007','2008','2009','2010','2011','2012','2013','2014', '2015', '2016', '2017']
+years   = ['1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006',
+           '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017']
 
 for i in range(0, len(years)):
     currName = years[i]
